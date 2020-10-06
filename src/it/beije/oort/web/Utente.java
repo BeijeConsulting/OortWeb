63ab8af1a62b608f87384cecb09c13d5f48ac005
ivfly <iv0fly@gmail.com>
2020-10-06 10:08:43 +0200

    useBean

---
+++ src/it/beije/oort/web/Utente.java
@@ -0,0 +1,79 @@
+package it.beije.oort.web;
+
+public class Utente {
+	
+	private int id;
+	
+	private String nome;
+	
+	private String cognome;
+	
+	private String telefono;
+	
+	private String email;
+	
+	
+	public Utente() {}
+	
+	public Utente(String nome, String cognome, String telefono) {
+		this(nome, cognome, telefono, "");
+	}
+	
+	public Utente(String nome, String cognome, String telefono, String email) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.telefono = telefono;
+		this.email = email;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+//	public String getNominativo() { //nominativo
+//		return this.nome + " " + this.cognome;
+//	}
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder("contatto [");
+		builder.append("nome : ").append(this.nome)
+			.append(" - cognome : ").append(this.cognome)
+			.append(" - telefono : ").append(this.telefono)
+			.append(" - email : ").append(this.email).append("]");
+		
+		return builder.toString();
+	}
+	
+}
